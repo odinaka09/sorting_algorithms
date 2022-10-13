bda3bdc6a6f1d350bda0cbe51ef761d5d9c8e8f2
odinaka09 <odinakanwobodo28@gmail.com>
2022-10-13 23:40:24 +0100

    the remaining

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,56 +1,57 @@
 #include "sort.h"
-
-
 /**
- * quicksort - Implementation fo selection sorting
- *@first: index of the first num
- *@last: index of the last number
- *Author: Thaoban Abdrasheed Ola
+ * quick_sort - function that sorts an array
+ * of integers in ascending order using the
+ * quick sort algorithm
  *
- *Return: nothing to return
+ * @array: input arrray
+ * @size: size of the array
+ * Return: no return
  */
-void quicksort(int *array, int first, int last)
-{
-   int i, j, pivot, temp, reserve;
-
-   reserve = last + 1;
-   if (first < last)
-   {
-      pivot = last;
-      i = first;
-      j = last;
-      while (i < j)
-      {
-         while (array[i] <= array[pivot] && i < last)
-            i++;
-         while (array[j] > array[pivot])
-            j--;
-         if (i < j)
-         {
-            temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-            print_array(array, reserve);
-         }
-      }
-      temp = array[pivot];
-      array[pivot] = array[j];
-      array[j] = temp;
-      quicksort(array, first, j - 1);
-      quicksort(array, j + 1, last);
-   }
-}
-
-/**
- * quick_sort - Implementation of quick-sorting algo
- *@array: pointer to the array of integers
- *@size: size of the array
- *Author: Thaoban Abdrasheed
- *
- *Return: nothing to return
- */
-
 void quick_sort(int *array, size_t size)
 {
-   quicksort(array, 0, size - 1);
+	_qsort(array, 0, size - 1, size);
+}
+/**
+ * _qsort - auxiliar function for the
+ * quick_sort function
+ * @a: input arrray
+ * @low: index for the first element
+ * @high: index for the last element
+ * @size: size of the array
+ * Return: no return
+ */
+void _qsort(int *a, int low, int high, int size)
+{
+	int p, w, i;
+	int tmp;
+
+	if (low < high)
+	{
+		p = high;
+		w = low;
+		for (i = low; i < high; i++)
+		{
+			if (a[i] < a[p])
+			{
+				if (i != w)
+				{
+					tmp = a[i];
+					a[i] = a[w];
+					a[w] = tmp;
+					print_array(a, size);
+				}
+				w++;
+			}
+		}
+		if (w != p && a[w] != a[p])
+		{
+			tmp = a[w];
+			a[w] = a[p];
+			a[p] = tmp;
+			print_array(a, size);
+		}
+		_qsort(a, low, w - 1, size);
+		_qsort(a, w + 1, high, size);
+	}
 }
